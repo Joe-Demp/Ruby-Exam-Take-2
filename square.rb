f818f2664e572f221233bb7d0edd44642a247724
Joe-Demp <dempseyjoseph96@yahoo.com>
2019-11-29 18:00:13 +0000

    Tested Square, all passed

--- square.rb
+++ square.rb
@@ -22,5 +22,5 @@ class Square
       :a => 1, :b => 2, :c => 3, :d => 4,
       :e => 5, :f => 6, :g => 7, :h => 8,
   }
-  I_TO_SYMBOL = [:a, :b, :c, :d, :e, :f, :g, :h]
+  I_TO_SYMBOL = [nil, :a, :b, :c, :d, :e, :f, :g, :h]
 end
--- square_test.rb
+++ square_test.rb
@@ -1,15 +1,33 @@
 require 'test/unit'
+require_relative 'square.rb'
 
 class SquareTest < Test::Unit::TestCase
   def setup
-    # Do nothing
+    @square_a = Square.new(:e, 6)
+    @square_b = Square.new(5, 6)
+    @square_c = Square.new(:e, 6)
+
+    @square_d = Square.new(:b, 2)
   end
 
-  def teardown
-    # Do nothing
+  def test_initialize
+    assert_equal(:e, @square_b.column)
+    assert_equal(6, @square_b.row)
+
+    assert_equal(:b, @square_d.column)
+    assert_equal(2, @square_d.row)
   end
 
-  def test
-    pend 'Not implemented'
+  def test_equality
+    assert(@square_a == @square_c)
+    assert(@square_a == @square_b)
+
+    assert_false(@square_c == @square_d)
+  end
+
+  def test_column_i
+    assert_equal(5, @square_a.column_i)
+    assert_equal(5, @square_b.column_i)
+    assert_equal(2, @square_d.column_i)
   end
 end
